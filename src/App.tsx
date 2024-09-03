d2a71dc897d87f49fa6a7588671ab93ed2d593ee
Henriquedearaujo <ricke_cris@hotmail.com>
2024-09-03 13:24:22 -0300

    Criando e estilizando header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { GlobalCss } from './styles'
+import Header from './components/Header'
+import { Container, GlobalCss } from './styles'
 
 function App() {
   return (
     <>
       <GlobalCss />
-      <div>olá mumdo</div>
+      <Container>
+        <Header />
+      </Container>
     </>
   )
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,31 @@
+import { HeaderBar, LinkCart, Links, LinksItem } from './styles'
+
+import logo from '../../assets/images/logo.svg'
+import carrinho from '../../assets/images/carrinho.svg'
+
+const Header = () => (
+  <HeaderBar>
+    <div>
+      <img src={logo} alt="EPLAY"></img>
+      <nav>
+        <Links>
+          <LinksItem>
+            <a href="#">Categorias</a>
+          </LinksItem>
+          <LinksItem>
+            <a href="#">Novidades</a>
+          </LinksItem>
+          <LinksItem>
+            <a href="#">Promoçaes</a>
+          </LinksItem>
+        </Links>
+      </nav>
+    </div>
+    <LinkCart href="#">
+      0 - produto(s)
+      <img src={carrinho} alt="Carrinho"></img>
+    </LinkCart>
+  </HeaderBar>
+)
+
+export default Header
